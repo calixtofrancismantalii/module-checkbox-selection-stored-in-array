49477a9e02868c07cffe881dfe273856e7a281ed
Calixto Mantal <cyriesmadrid@gmail.com>
2024-02-25 14:58:52 -0700

    Done Selectetion of Exhibitors

--- boothAssignment.js
+++ boothAssignment.js
@@ -1,12 +1,7 @@
-// When the DOM (HTML structure) is ready
-// document.addEventListener("DOMContentLoaded");
 $(document).ready(function () {
-  // First, we can create references to DOM elements on page start,
-  // which we'll reuse instead of looking them up every time we perform a search.
-  // As a convention, I like variables holding DOM elements to start with $,
-  // so that I know what I'm dealing with in the blink of an eye 👀
-  let $input = document.getElementById("myInput");
-  let $table = document.getElementById("myTable");
+  let $input = document.getElementById("inputSearch");
+  let $table = document.getElementById("exhibitorsTable");
+
   // Only select the <tr>s inside the <tbody> (double $ -> multiple elements)
   let $$tr = $table.querySelectorAll("tbody tr");
 
@@ -17,18 +12,11 @@ $(document).ready(function () {
       $$tr[i].querySelector("td").innerText
     );
   }
-  // When typing or pasting text, perform a search
+
+  // When typing or pasting text, perform a search function
   $input.addEventListener("input", performSearch);
 
-  //   function performSearch() {
-  //     var filter = normalizeStr(this.value);
-  //     for (var i = 0; i < $$tr.length; i++) {
-  //       var isMatch = $$tr[i].normalizedValue.includes(filter);
-  //       // Toggle a 'visible' class
-  //       $$tr[i].classList[isMatch ? "add" : "remove"]("visible");
-  //     }
-  //   }
-
+  //search function
   function performSearch() {
     var filter = normalizeStr(this.value),
       resultCount = 0;
@@ -38,53 +26,52 @@ $(document).ready(function () {
       if (isMatch) {
         resultCount++;
       }
-
       $$tr[i].classList[isMatch ? "add" : "remove"]("visible");
     }
-
-    var showNoResultsMessage = resultCount === 0 && filter.length > 0;
+    // var showNoResultsMessage = resultCount === 0 && filter.length > 0;
     // $noResults.classList[showNoResultsMessage ? "add" : "remove"]("visible");
   }
-
   // Creating a reusable function will allow us to make
-  // changes to it only in one place 👍
+  // changes to it only in one place
   function normalizeStr(str) {
     return str.toUpperCase().trim();
   }
-});
 
-function myfun(e) {
-  //   const chkbx = document.getElementsByName("chkbx[]");
+  const selectedExhibitors = $("#selectedExhibitors");
+  let selectedOptions = [];
 
-  $("input:checkbox").change(function () {
-    let isChecked = $("input:checkbox:checked")
-      .map(function () {
-        return this.value;
-      })
-      .toArray();
-
-    // isChecked = isChecked.toString();
-
-    // // if string contains ',', then run this function
-    // while (isChecked.indexOf(",") > -1) {
-    //   //then replace the commas
-    //   isChecked = isChecked.replace(",", " ");
-    // }
-
-    // $("#view").text(isChecked);
-
-    let table = "<tr>",
-      perrow = 1,
-      cells = 0;
-
-    isChecked.forEach((value) => {
-      table += `<td>${value} <span onclick="alert('test')" style="font-size: 12px; cursor:pointer;"><i class="fa-solid fa-xmark"></i></span></td>`;
-      cells++;
-      if (cells % perrow == 0 && cells != isChecked.length) {
-        table += "<tr></tr>";
+  $(".checkbox").change(function () {
+    const value = $(this).val();
+    if ($(this).prop("checked")) {
+      selectedOptions.push(value);
+    } else {
+      const index = selectedOptions.indexOf(value);
+      if (index !== -1) {
+        selectedOptions.splice(index, 1);
       }
-    });
-    table += "</tr>";
-    document.getElementById("selectedData").innerHTML = table;
+    }
+    updateTable();
   });
-}
+
+  //update table of selected exhbitor whenever it clicks
+  function updateTable() {
+    selectedExhibitors.empty();
+    $.each(selectedOptions, function (index, option) {
+      const newRow = $("<tr>");
+      const cell = $("<td>").text(option).appendTo(newRow);
+      const lbl = $(
+        "<label style='font-size: 12px; cursor:pointer; display: inline-block; margin-left:5px'>"
+      ).appendTo(cell);
+      lbl.html('<i class="fa-solid fa-xmark"></i>');
+      cell.click(function () {
+        const index = selectedOptions.indexOf(option);
+        if (index !== -1) {
+          selectedOptions.splice(index, 1);
+          $('.checkbox[value="' + option + '"]').prop("checked", false);
+          updateTable();
+        }
+      });
+      newRow.appendTo(selectedExhibitors);
+    });
+  }
+});
